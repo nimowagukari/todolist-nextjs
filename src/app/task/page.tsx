e0bafa6e04dc66dca33b49a814b3138b17901032
nimowagukari <nimowagukari@outlook.com>
2024-05-14 13:55:37 +0000

    API Route への Fetch 時のベース URL を修正

---
+++ src/app/task/page.tsx
@@ -0,0 +1,27 @@
+import { Task } from "../api/task/data";
+
+async function getData() {
+  const base_url =
+    "VERCEL_URL" in process.env
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000";
+  const res = await fetch(`${base_url}/api/task`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data: Task[] = await getData();
+  return (
+    <>
+      <div>
+        <h1>task</h1>
+        <p>{JSON.stringify(data)}</p>
+      </div>
+    </>
+  );
+}
