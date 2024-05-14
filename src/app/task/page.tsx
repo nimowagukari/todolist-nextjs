04f82182d2bd100541f1c564c8cbefa672353926
nimowagukari <nimowagukari@outlook.com>
2024-05-14 14:13:27 +0000

    キャッシュしないよう修正

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -5,7 +5,7 @@ async function getData() {
     "VERCEL_URL" in process.env
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000";
-  const res = await fetch(`${base_url}/api/task`);
+  const res = await fetch(`${base_url}/api/task`, { cache: "no-store" });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
