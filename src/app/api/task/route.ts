2d12e2e57e0fa3add4ec5009986acd71484c142b
nimowagukari <nimowagukari@outlook.com>
2024-07-03 12:11:33 +0000

    select クエリ結果を id 昇順で返すよう修正

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -4,7 +4,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const tasks = await prisma.task.findMany();
+  const tasks = await prisma.task.findMany({
+    orderBy: [{ id: "asc" }],
+  });
   const res = NextResponse.json({ tasks: tasks }, { status: 200 });
   return res;
 }
